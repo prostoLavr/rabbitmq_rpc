68ed612ac00d9b5c227485f5574237d1a70780d5
Lawrence Naumov <prostolawr@gmail.com>
2022-08-16 21:51:24 +0800

    Update

---
+++ rabbitmq_rpc/__init__.py
@@ -0,0 +1,3 @@
+from rabbitmq_rpc.client import RpcClient, RpcClientWorker
+from rabbitmq_rpc.server import declare_to_receive
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+
+requirements = ['loguru', 'pika']
+
+
+setup(name='rabbitmq_rpc',
+      version='0.1',
+      url='https://github.com/prostolavr/rabbitmq_rpc',
+      license='MIT',
+      author='Lawrence Naumov',
+      author_email='prostoLawr@gmail.com',
+      packages=find_packages(),
+      install_requires=requirements,
+      classifiers=[
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+      ],
+      zip_safe=False)
+
